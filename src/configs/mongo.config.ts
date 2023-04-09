90e93da8320e054174bb6768c8fb1b66c654ebd0
yarikLev04 <91241935+yarikLev04@users.noreply.github.com>
2023-04-09 14:28:21 +0300

    Add mongo config

---
+++ src/configs/mongo.config.ts
@@ -0,0 +1,26 @@
+import { ConfigService } from '@nestjs/config';
+import { MongooseModuleOptions } from '@nestjs/mongoose';
+import { ConnectOptions } from 'mongoose';
+
+export const getMongoConfig = async (
+	configService: ConfigService
+): Promise<MongooseModuleOptions> => {
+	return {
+		uri: getMongoString(configService),
+		...getMongoOptions(),
+	};
+};
+
+const getMongoString = (configService: ConfigService) =>
+	'mongodb://' +
+	configService.get('MONGO_LOGIN') +
+	':' +
+	configService.get('MONGO_PASSWORD') +
+	'@' +
+	configService.get('MONGO_HOST') +
+	':' +
+	configService.get('MONGO_PORT') +
+	'/' +
+	configService.get('MONGO_AUTHDATABASE');
+
+const getMongoOptions = (): ConnectOptions => ({});
