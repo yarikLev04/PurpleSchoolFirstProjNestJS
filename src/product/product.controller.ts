5b868347884a8f322fd59c55ac523ec78dbe429e
yarikLev04 <91241935+yarikLev04@users.noreply.github.com>
2023-04-15 21:15:34 +0300

    Finish product logic

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -16,13 +16,14 @@ import { FindProductDto } from './dto/find-product.dto';
 import { CreateProductDto } from './dto/create-product.dto';
 import { ProductService } from './product.service';
 import { PRODUCT_NOT_FOUND_ERROR } from './product.consts';
+import { IdValidationPipe } from '../pipes/id-validation.pipe';
 
 @Controller('product')
 export class ProductController {
 	constructor(private productService: ProductService) {}
 
 	@Get(':id')
-	async get(@Param('id') id: string) {
+	async get(@Param('id', IdValidationPipe) id: string) {
 		const product = await this.productService.findById(id);
 
 		if (!product) {
@@ -45,7 +46,10 @@ export class ProductController {
 	}
 
 	@Patch(':id')
-	async update(@Param('id') id: string, @Body() dto: ProductModel) {
+	async update(
+		@Param('id', IdValidationPipe) id: string,
+		@Body() dto: ProductModel
+	) {
 		const updatedProduct = await this.productService.updateById(id, dto);
 
 		if (!updatedProduct) {
@@ -56,7 +60,7 @@ export class ProductController {
 	}
 
 	@Delete(':id')
-	async delete(@Param('id') id: string) {
+	async delete(@Param('id', IdValidationPipe) id: string) {
 		const deletedProduct = await this.productService.delete(id);
 
 		if (!deletedProduct) {
